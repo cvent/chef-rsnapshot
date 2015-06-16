70b900a5a30ce06767d652944f369157c3da84d6
Morley, Jonathan <jmorley@cvent.com>
2015-06-16 16:04:28 -0400

    Better defaults for intervals

--- attributes/rsnapshot.rb
+++ attributes/rsnapshot.rb
@@ -1,8 +1,10 @@
 default['rsnapshot']['root'] = '/u/backup/snapshots'
 
-default['rsnapshot']['intervals']['hourly'] = 6
-default['rsnapshot']['intervals']['daily'] = 7
-default['rsnapshot']['intervals']['weekly'] = 4
+default['rsnapshot']['intervals'] = {
+  'hourly' => 6,
+  'daily' => 7,
+  'weekly' => 4
+}
 
 default['rsnapshot']['verbosity'] = 2
 default['rsnapshot']['loglevel'] = 3
