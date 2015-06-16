52ea5639bde996e992480ef7f1bef05c7448640d
Morley, Jonathan <jmorley@cvent.com>
2015-06-16 15:41:12 -0400

    Add more attributes to rsnapshot.conf, add crontab entries

--- attributes/rsnapshot.rb
+++ attributes/rsnapshot.rb
@@ -1,1 +1,8 @@
-rsnapshot Mash.new unless attribute?(:rsnapshot)
+default['rsnapshot']['root'] = '/u/backup/snapshots'
+
+default['rsnapshot']['intervals']['hourly'] = 6
+default['rsnapshot']['intervals']['daily'] = 7
+default['rsnapshot']['intervals']['daily'] = 4
+
+default['rsnapshot']['verbosity'] = 2
+default['rsnapshot']['loglevel'] = 3
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,5 +1,12 @@
-package "rsnapshot"
+package 'rsnapshot'
 
-template "/etc/rsnapshot.conf" do
-  source "rsnapshot.conf.erb"
+template '/etc/rsnapshot.conf' do
+  source 'rsnapshot.conf.erb'
+end
+
+node['rsnapshot']['intervals'].keys.each do |period|
+  cron "rsnapshot-#{period}" do
+    time period.intern
+    command "rsnapshot #{period}"
+  end
 end
