1f34da37a41c7e71a440761d6599b443237d335c
Morley, Jonathan <jmorley@cvent.com>
2015-06-16 17:45:53 -0400

    Remove default intervals

--- attributes/rsnapshot.rb
+++ attributes/rsnapshot.rb
@@ -1,10 +1,4 @@
 default['rsnapshot']['root'] = '/u/backup/snapshots'
 
-default['rsnapshot']['intervals'] = {
-  'hourly' => 6,
-  'daily' => 7,
-  'weekly' => 4
-}
-
 default['rsnapshot']['verbosity'] = 2
 default['rsnapshot']['loglevel'] = 3
