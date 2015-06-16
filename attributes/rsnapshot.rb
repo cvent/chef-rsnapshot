91b0dd13ca26d4ca96f77ac94f7ded9ae0ba9262
Morley, Jonathan <jmorley@cvent.com>
2015-06-16 16:00:59 -0400

    Fix copy paste error

--- attributes/rsnapshot.rb
+++ attributes/rsnapshot.rb
@@ -2,7 +2,7 @@ default['rsnapshot']['root'] = '/u/backup/snapshots'
 
 default['rsnapshot']['intervals']['hourly'] = 6
 default['rsnapshot']['intervals']['daily'] = 7
-default['rsnapshot']['intervals']['daily'] = 4
+default['rsnapshot']['intervals']['weekly'] = 4
 
 default['rsnapshot']['verbosity'] = 2
 default['rsnapshot']['loglevel'] = 3
